8264807ba2353a02fc909efcc82a388f31e0bfe4
Diego Collares <diego.collares@gmail.com>
2015-09-08 22:35:07 -0300

    submit http.post api

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,11 +1,18 @@
 var app = angular.module('minmax', [ ]);
 
-app.controller('MainMaxCtrl', function($scope){
+app.controller('MainMaxCtrl', function($scope, $http){
 	$scope.formModel = {};
 
 	$scope.onSubmit = function (){
 		console.log("Hey im submitted!");
 		console.log($scope.formModel);
+
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+		success(function(data){
+			console.log("--------> OK ")
+		}).error(function(data){
+			console.log("Fail <-------- ")
+		})
 	}
 
 })
