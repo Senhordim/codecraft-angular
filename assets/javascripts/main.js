090c2e6bb404918b5146c9209ce8f252c96e4310
Diego Collares <diego.collares@gmail.com>
2015-09-10 15:03:06 -0300

    intall angular-ladda and config

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,21 +1,25 @@
 var app = angular.module('minmax', [ 
-	'jcs-autoValidate'
+	'jcs-autoValidate',
+	'angular-ladda'
 ]);
 
 app.controller('MainMaxCtrl', function($scope, $http){
 	$scope.formModel = {};
+	$scope.submitting = false;
 
 	$scope.onSubmit = function (){
-
+		$scope.submitting = true;
 		console.log("Hey im submitted!");
 		console.log($scope.formModel);
 
-		// $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-		// success(function(data){
-		// 	console.log("--------> OK ")
-		// }).error(function(data){
-		// 	console.log("Fail <-------- ")
-		// });
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+		success(function(data){
+			console.log("--------> OK ")
+			$scope.submitting = false;
+		}).error(function(data){
+			console.log("Fail <-------- ")
+			$scope.submitting = false;
+		});
 
 
 	}
